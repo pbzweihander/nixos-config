32b50f61a227c0458e7f8b8b2bceb947d0572786
pbzweihander <pbzweihander@protonmail.com>
2024-05-17 00:42:20 +0900

    Setup git

--- home/basic.nix
+++ home/basic.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./fish
+    ./git
     ./helix
   ];
 
---
+++ home/git/default.nix
@@ -0,0 +1,66 @@
+{
+  programs.git = {
+    enable = true;
+    delta = {
+      enable = true;
+      options = {
+        features = "decorations";
+        pager = "less -+XF -Qc";
+        side-by-side = true;
+        plus-style = "syntax green";
+        minus-style = "syntax red";
+      };
+    };
+    extraConfigs = {
+      branch.autosetuprebase = "always";
+      color.ui = "auto";
+      core = {
+        editor = "hx";
+        pager = "less -+XF -Qc";
+        fscache = "yes";
+        autocrlf = "no";
+        quotepath = "no";
+        precomposeunicode = "yes";
+      } credential {
+        helper = "cache --timeout=3600";
+        "https://github.com".username = "pbzweihander";
+      };
+      diff = {
+        renames = "copies";
+        tool = "vimdiff";
+        compactionHeuristic = true;
+      };
+      difftool.prompt = "no";
+      fetch.prune = "yes";
+      gpg.format = "ssh";
+      init.defaultBranch = "main";
+      log.date = "iso8601";
+      merge = {
+        tool = "vimdiff";
+        conflictstyle = "diff3";
+      };
+      pager = {
+        diff = "delta";
+        log = "delta";
+        reflog = "delta";
+        show = "delta";
+        stash = "false";
+      };
+      push.default = "simple";
+    };
+    ignores = [
+      ".node-version"
+      ".python-version"
+      ".terraform-version"
+      ".tool-versions"
+      ".rtx.toml"
+    ];
+    signing = {
+      key = "~/.ssh/id_ed25519.pub";
+      signByDefault = true;
+    };
+    userEmail = "pbzweihander@protonmail.com";
+    userName = "pbzweihander";
+  };
+}
+
