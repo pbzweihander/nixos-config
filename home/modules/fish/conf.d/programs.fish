5f79fba18f94293a612a7d3f52979552c92a169e
pbzweihander <pbzweihander@protonmail.com>
2024-05-19 17:35:03 +0900

    Fix mise

--- home/modules/fish/conf.d/programs.fish
+++ home/modules/fish/conf.d/programs.fish
@@ -3,4 +3,5 @@ if type -q mise
     set -lxp fish_user_paths ~/.local/share/mise/installs/usage/latest/bin
     mise activate fish | source
     mise completion fish | source
+    mise use -g usage >/dev/null
 end
