c95b63cac7b5ef957450904bedc31f072b7a9a84
pbzweihander <pbzweihander@protonmail.com>
2024-05-31 11:58:21 +0900

    Add helm

--- modules/kubectl.nix
+++ modules/kubectl.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ kubectl kubectx ];
+  environment.systemPackages = with pkgs; [
+    kubectl
+    kubectx
+    kubernetes-helm
+    kubernetes-helmPlugins.helm-diff
+    kubernetes-helmPlugins.helm-s3
+  ];
 }
