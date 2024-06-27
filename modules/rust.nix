e0bd31520016c5405b9d27f0b7abf58e1c1f27ba
pbzweihander <pbzweihander@protonmail.com>
2024-06-27 17:44:45 +0900

    Add cargo and cargo-edit

--- modules/rust.nix
+++ modules/rust.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ rust-analyzer cargo-sort ];
+  environment.systemPackages = with pkgs; [
+    cargo
+    cargo-edit
+    cargo-sort
+    rust-analyzer
+  ];
 }
