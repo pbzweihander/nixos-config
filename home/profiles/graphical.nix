538013ce071b1000c7a463ad7faf466cb54cfcda
pbzweihander <pbzweihander@protonmail.com>
2024-07-13 14:11:34 +0900

    Update KDE

--- home/profiles/graphical.nix
+++ home/profiles/graphical.nix
@@ -9,42 +9,21 @@
 
   programs.plasma = {
     enable = true;
-
     workspace = {
       clickItemTo = "select";
       wallpaperPictureOfTheDay.provider = "apod";
     };
-
-    panels = [{
-      location = "right";
-      widgets = [
-        { name = "org.kde.plasma.kickoff"; }
-        {
-          name = "org.kde.plasma.icontasks";
-          config = {
-            General.launchers =
-              [ "applications:firefox" "applications:org.kde.dolphin.desktop" ];
-          };
-        }
-        "org.kde.plasma.marginsseparator"
-        "org.kde.plasma.systemtray"
-        {
-          digitalClock = {
-            format = "MM/dd";
-            time.format = "24h";
-          };
-        }
-      ];
-    }];
-
     shortcuts = {
       ksmserver = { "Lock Session" = [ "Screensaver" "Meta+Esc" ]; };
       "services/Alacritty.desktop"."New" = "Meta+Return";
     };
-
     configFile = {
+      "dolphinrc"."DetailsMode"."ExpandableFolders" = false;
+      "kdeglobals"."General"."TerminalApplication" = "alacritty";
       "kwinrc"."Effect-overview"."BorderActivate" = 9;
       "kwinrc"."Wayland"."InputMethod[$e]" =
+        "/etc/profiles/per-user/pbzweihander/share/applications/org.fcitx.Fcitx5.desktop";
+      "kwinrc"."Wayland"."InputMethodx5b$ex5d" =
         "/etc/profiles/per-user/pbzweihander/share/applications/org.fcitx.Fcitx5.desktop";
       "ksmserverrc"."General"."confirmLogout" = false;
     };
