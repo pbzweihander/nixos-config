b8d6e2b93f7a7d5a3518844a3a133ce380b3754e
pbzweihander <pbzweihander@protonmail.com>
2024-08-02 04:24:43 +0900

    Add opentrack flag

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -15,5 +15,19 @@ final: prev:
       "-DSDK_WINE=ON"
       "-DONNXRuntime_INCLUDE_DIR=${final.onnxruntime.dev}/include"
     ];
+    desktopItems = [
+      (final.makeDesktopItem rec {
+        name = "opentrack";
+        exec = "opentrack -platform xcb";
+        icon = final.fetchurl {
+          url =
+            "https://github.com/opentrack/opentrack/raw/opentrack-2023.3.0/gui/images/opentrack.png";
+          sha256 = "0d114zk78f7nnrk89mz4gqn7yk3k71riikdn29w6sx99h57f6kgn";
+        };
+        desktopName = name;
+        genericName = "Head tracking software";
+        categories = [ "Utility" ];
+      })
+    ];
   });
 }
