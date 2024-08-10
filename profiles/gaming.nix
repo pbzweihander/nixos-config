42749977cb38a0e6e705699f6ee54e1e73120542
pbzweihander <pbzweihander@protonmail.com>
2024-08-10 21:13:37 +0900

    Update steam packages

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -6,14 +6,11 @@
     remotePlay.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
     gamescopeSession.enable = true;
+    protontricks.enable = true;
     fontPackages = with pkgs; [ wqy_zenhei ];
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
   programs.gamescope.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    gamescope
-    protontricks
-    protonup-qt
-    vesktop
-  ];
+  environment.systemPackages = with pkgs; [ vesktop gamescope ];
 }
