f4cd48365471bacb15a7e4a2110ca7c0e0168592
pbzweihander <pbzweihander@protonmail.com>
2025-01-06 19:48:36 +0900

    Fix devshell

--- home/modules/fish/functions/ds.fish
+++ home/modules/fish/functions/ds.fish
@@ -1,15 +1,15 @@
 function ds
     argparse --min-args=1 'v/version=?' e/edit a/allow -- $argv; or return
-    set -q argv[2]; or set argv[2] .
-    cp -ir ~/nixos-config/devshells/$argv[1]/.* $argv[2]; or return
+    cp -ir ~/nixos-config/devshells/$argv[1]/.* .; or return
+    sed -i "s/<path>/$(pwd | sed 's/\\//\\\\\\//g')/g" .envrc; or return
     if set -q _flag_v
         set _v (string sub --start 2 $_flag_v)
-        sed -i "s/<version>/$_v/g" $argv[2]/.flake/flake.nix
+        sed -i "s/<version>/$_v/g" .flake/flake.nix; or return
     end
     if set -q _flag_e
-        $EDITOR $argv[2]/.flake/flake.nix
+        $EDITOR .flake/flake.nix
     end
     if set -q _flag_a
-        direnv allow $argv[2]
+        direnv allow
     end
 end
