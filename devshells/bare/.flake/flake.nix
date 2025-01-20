b583f47d3a6578321459b4eb8537db9caac2dbdc
pbzweihander <pbzweihander@protonmail.com>
2025-01-20 15:32:39 +0900

    Add bare devshell

---
+++ devshells/bare/.flake/flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShells.default =
+          pkgs.mkShell { nativeBuildInputs = with pkgs; [ ]; };
+      });
+}
