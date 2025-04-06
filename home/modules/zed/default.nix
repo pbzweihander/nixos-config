041c58082ccf1f2ba9d3f1994f42389cc705c9af
pbzweihander <pbzweihander@protonmail.com>
2025-04-07 00:42:22 +0900

    Add zed keymap

--- home/modules/zed/default.nix
+++ home/modules/zed/default.nix
@@ -1,6 +1,7 @@
 {
   home.file = {
     ".config/zed/settings.json".source = ./settings.json;
+    ".config/zed/keymap.json".source = ./keymap.json;
     ".config/zed/themes/catppuccin.json".source = ./themes/catppuccin.json;
   };
 }
