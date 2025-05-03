51e250e7b0d5839385894980fd07d6e8ac7b9d02
pbzweihander <pbzweihander@protonmail.com>
2025-05-03 23:22:06 +0900

    Add go devshell

---
+++ devshells/go/.flake/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [];
+      pkgs = import nixpkgs {inherit system overlays;};
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          go
+          gopls
+          golangci-lint
+          golangci-lint-langserver
+        ];
+      };
+    });
+}
