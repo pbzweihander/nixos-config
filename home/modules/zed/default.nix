746e7ca0f1b05f8f0ff459b81820679d6b34c20a
pbzweihander <pbzweihander@protonmail.com>
2025-05-12 12:53:22 +0900

    Add zed dimidium theme

--- home/modules/zed/default.nix
+++ home/modules/zed/default.nix
@@ -3,5 +3,6 @@
     ".config/zed/settings.json".source = ./settings.json;
     ".config/zed/keymap.json".source = ./keymap.json;
     ".config/zed/themes/catppuccin.json".source = ./themes/catppuccin.json;
+    ".config/zed/themes/dimidium.json".source = ./themes/dimidium.json;
   };
 }
