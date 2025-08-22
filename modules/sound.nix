da1cdae324f472f08d7fb5a76a45376ba15a5d04
pbzweihander <pbzweihander@protonmail.com>
2025-08-22 12:27:06 +0900

    Add ardour

--- modules/sound.nix
+++ modules/sound.nix
@@ -1,12 +1,18 @@
+{ pkgs, ... }:
 {
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     audio.enable = true;
     pulse.enable = true;
+    jack.enable = true;
     alsa = {
       enable = true;
       support32Bit = true;
     };
   };
+  environment.systemPackages = with pkgs; [
+    ardour
+    rtaudio
+  ];
 }
--- modules/users.nix
+++ modules/users.nix
@@ -10,10 +10,27 @@
     extraGroups = [
       "wheel"
       "dialout"
+      "audio"
+      "rtkit"
     ];
     shell = pkgs.fish;
     uid = 1000;
     createHome = true;
     openssh.authorizedKeys.keyFiles = [ ../authorized_keys ];
   };
+
+  security.pam.loginLimits = [
+    {
+      domain = "@audio";
+      item = "memlock";
+      type = "-";
+      value = "unlimited";
+    }
+    {
+      domain = "@audio";
+      item = "rtprio";
+      type = "-";
+      value = "99";
+    }
+  ];
 }
