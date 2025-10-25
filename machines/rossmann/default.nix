21c1748bfdff5342cf6f318908bed2657acf5d99
pbzweihander <pbzweihander@protonmail.com>
2025-10-26 03:54:10 +0900

    maybe fix cachix

--- machines/rossmann/default.nix
+++ machines/rossmann/default.nix
@@ -99,12 +99,12 @@ in
   ];
 
   nix.settings = {
-    substituters = [
+    extra-substituters = [
       "https://nix-gaming.cachix.org"
       "https://nix-citizen.cachix.org"
       "https://ezkea.cachix.org"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
       "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
