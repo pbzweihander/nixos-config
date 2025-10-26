9e480f69c71f38dde5ac24f47cbeddc5b32b16b1
pbzweihander <pbzweihander@protonmail.com>
2025-10-26 23:43:40 +0900

    Fix

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -1,11 +1,4 @@
 { pkgs, ... }:
-let
-  patchedBwrap = pkgs.bubblewrap.overrideAttrs (o: {
-    patches = (o.patches or [ ]) ++ [
-      ./bwrap.patch
-    ];
-  });
-in
 {
   programs = {
     steam = {
@@ -15,55 +8,16 @@ in
       protontricks.enable = true;
       fontPackages = with pkgs; [ wqy_zenhei ];
       extraCompatPackages = with pkgs; [ proton-ge-bin ];
-      # https://github.com/NixOS/nixpkgs/issues/217119
-      package = pkgs.steam.override {
-        buildFHSEnv = (
-          args:
-          (
-            (pkgs.buildFHSEnv.override {
-              bubblewrap = patchedBwrap;
-            })
-            (
-              args
-              // {
-                extraBwrapArgs = (args.extraBwrapArgs or [ ]) ++ [ "--cap-add ALL" ];
-              }
-            )
-          )
-        );
-      };
     };
-    gamescope = {
-      enable = true;
-      capSysNice = true;
-    };
+    gamescope.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
-    gamescope
-    unstable.nexusmods-app-unfree
+    lutris
     prismlauncher
     steamtinkerlaunch
     vesktop
 
-    # https://github.com/NixOS/nixpkgs/issues/292620
-    # https://github.com/NixOS/nixpkgs/issues/217119
-    patchedBwrap
-    (lutris.override {
-      buildFHSEnv = (
-        args:
-        (
-          (pkgs.buildFHSEnv.override {
-            bubblewrap = patchedBwrap;
-          })
-          (
-            args
-            // {
-              extraBwrapArgs = (args.extraBwrapArgs or [ ]) ++ [ "--cap-add ALL" ];
-            }
-          )
-        )
-      );
-    })
+    unstable.nexusmods-app-unfree
   ];
 }
