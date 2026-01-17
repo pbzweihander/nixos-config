a54b6109f0e2046b6f2bd4a6307a17e02f820820
pbzweihander <pbzweihander@protonmail.com>
2026-01-17 22:11:35 +0900

    Add japanese ime

--- machines/rossmann/home.nix
+++ machines/rossmann/home.nix
@@ -23,4 +23,6 @@
   home.file.".links/nix-citizen-wine-astral/bin".source = "${
     inputs.nix-citizen.packages.${pkgs.stdenv.hostPlatform.system}.wine-astral
   }/bin";
+
+  i18n.inputMethod.fcitx5.addons = with pkgs; [ fcitx5-mozc ];
 }
