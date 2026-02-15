083df3a19354caf5cab16a0e21c9303a1c308dcd
pbzweihander <pbzweihander@protonmail.com>
2026-02-15 16:24:21 +0900

    Update ssh keys

--- modules/users.nix
+++ modules/users.nix
@@ -14,6 +14,11 @@
     shell = pkgs.fish;
     uid = 1000;
     createHome = true;
-    openssh.authorizedKeys.keyFiles = [ ../authorized_keys ];
+    openssh.authorizedKeys.keyFiles = [
+      (builtins.fetchurl {
+        url = "https://github.com/pbzweihander.keys";
+        sha256 = "sha256:1zydkj8f4xpsv2p1vx8kndmpwlhlhxi7l9hmr55n7y5glszn05ci";
+      })
+    ];
   };
 }
