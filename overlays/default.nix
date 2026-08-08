46d968ed734a27d5445ae679120ad63a328eb873
pbzweihander <pbzweihander@protonmail.com>
2026-08-08 20:45:59 +0900

    Remove special netbird flake

--- overlays/default.nix
+++ overlays/default.nix
@@ -40,9 +40,4 @@ final: prev: {
     inherit (prev.stdenv.hostPlatform) system;
     config.allowUnfree = true;
   };
-
-  netbird-update = import inputs.nixpkgs-netbird-update {
-    inherit (prev.stdenv.hostPlatform) system;
-    config.allowUnfree = true;
-  };
 }
